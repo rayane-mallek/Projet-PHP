b3f82c3916e67e19dd2b93c64e91d00d8eecbea4
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-12-11 21:24:40 +0100

    page details.php en cours

--- src/PHP/view/product/detail.php
+++ src/PHP/view/product/detail.php
@@ -1,3 +1,10 @@
-<?php
-    echo '<p> Product name ' . htmlentities($p->getName()) . '.</p>';
-?>
+<div class="article">
+    <div class="box_article">
+        <img src="<?php echo $p->getImage() ?>" class="img_article"></img>
+        <div class="black_bar"></div>
+        <h3><?php echo 'Article ' . htmlentities($p->getName()) ; ?></h3>
+        <p> <?= $p->getDescription() ?></p>
+        <p> <?= $p->getPrice() ?></p>
+    </div>
+
+</div>
