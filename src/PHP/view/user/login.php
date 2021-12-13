9bc7691519ef96426aabd2895d53138ced00eff4
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-12-13 12:10:05 +0100

    inscription réussie

--- src/PHP/view/user/login.php
+++ src/PHP/view/user/login.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once './PHP/config/BDD.php';
+    require_once './PHP/config/Conf.php';
 
   if ((isset($_SESSION['id']))){ //si une session existe déja (= utilisateur connecté) on redirige vers la page d'accueil
     header('Location: ./index.php');
@@ -9,7 +9,7 @@
 
   
   if(!empty($_POST)){ // Si la variable "$_Post" contient des informations alors on les traites
-    extract($_POST); //extrait les valeurs du form en 2 variables $email $mdp
+    extract($_POST); //extrait les valeurs du form en 2 variables $email $password
     
     $ok = true;
 
@@ -18,25 +18,25 @@
 
     //htmlentites = pour éviter les injections, trim = enleve les espaces au début et a la fin
     $email = htmlentities(strtolower(trim($email)));
-    $mdp = trim($mdp);
-
+    $password = trim($_POST['password']);
     if(empty($email)){ //test si email est vide
       $ok = false;
       $er_email = "L'email est vide";
     }
 
-    if(empty($mdp)){ //test si le mdp est vide
+    if(empty($password)){ //test si le password est vide
       $ok = false;
-      $er_mdp = "Le mot de passe est vide";
+      $er_password = "Le mot de passe est vide";
     }
       
         
       
 
-    $mdp= crypt($mdp, '$6$rounds=5000$nuitexpressSauveteurExpress$'); //on crypte le mdp avec la meme clé que pour l'inscription
+    $password= crypt($password, '$6$rounds=5000$phpprojet$'); //on crypte le password avec la meme clé que pour l'inscription
+    $token = bin2hex(random_bytes(12));
 
-    $req = $pdo->prepare("SELECT * FROM NDI__User WHERE email = :email AND motdepasse = :mdp"); 
-    $req->execute(array('email' => $email, 'mdp' => $mdp));
+    $req = Model::getPDO()->prepare("SELECT * FROM p__user WHERE email = :email AND password = :password"); 
+    $req->execute(array('email' => $email, 'password' => $password));
     $resultat = $req->fetch();
     //on test si les valeurs du formulaire correspondent a la bdd
 
@@ -48,9 +48,12 @@
     if ($ok){
      //si tout est valide, alors on charge une session avec les attributs de la requete
       $_SESSION['id'] = $resultat['id']; 
-      $_SESSION['pseudo'] = htmlentities($resultat['pseudo']); //htmlentities pour éviter les injections html/php
+      $_SESSION['username'] = htmlentities($resultat['username']); //htmlentities pour éviter les injections html/php
       $_SESSION['email'] = htmlentities($resultat['email']);
-      $_SESSION['date'] = htmlentities($resultat['date_creation']);
+
+      $req = $pdo->prepare("INSERT INTO NDI__User
+      SET username = :username, motdepasse = :motdepasse, email = :email, date_creation = :datecreation");
+      $req->execute(array('username' => $_SESSION['username'], 'password' => $password, 'email' => $email));
 
       header('Location: ./index.php'); //on redirige l'utilisateur vers la page d'accueil
       exit;
@@ -67,7 +70,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Connexion</title>
+    <title>Log in</title>
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
@@ -79,9 +82,9 @@
     <section class="clean-block clean-form dark" style="height: 830.188px;">
         <div class="container text-start" style="height: 459px;">
             <div class="block-heading" style="height: -5px;">
-                <h2 class="text-info" style="text-align: center;"><strong>Se connecter</strong></h2>
+                <h2 class="text-info" style="text-align: center;"><strong>Log in</strong></h2>
             </div>
-            <p style="text-align: center;">Connectez vous à votre compte SauveteurExpress<br></p>
+            <p style="text-align: center;"><br>Login form</p>
             <form method="post">
               <?php
               if (isset($er_email)){ //si $er_mail n'est pas vide, alors on l'affiche
@@ -90,19 +93,20 @@
               <?php
                 }
               ?>
-                <div class="mb-3"><label class="form-label" for="email"><strong>Adresse Email</strong><br></label>
-                  <input class="form-control item" type="email" id="email" placeholder="Adresse email" name="email" value="<?php if(isset($email)){ echo $email; }?>" required></div>
+                <div class="mb-3"><label class="form-label" for="email"><strong> Email</strong><br></label>
+                  <input class="form-control item" type="email" id="email" placeholder=" email" name="email" value="<?php if(isset($email)){ echo $email; }?>" required></div>
                   <?php
-                  if (isset($er_mdp)){ //si $er_mdp n'est pas vide, alors on l'affiche
+                  if (isset($er_password)){ //si $er_password n'est pas vide, alors on l'affiche
                   ?>
-                    <div><?= $er_mdp ?></div>
+                    <div><?= $er_password ?></div>
                   <?php
                     }
                   ?>
-                <div class="mb-3"><label class="form-label" for="password"><strong>Mot de passe <a href="./index.php?action=resetmdp">Mot de passe oublié</a></strong><br></label>
-                  <input class="form-control" type="password" id="password" name="mdp"></div>
-                <div class="mb-3" style="width: 435px;height: -65px;margin: 20px;padding: 0px;"></div><button class="btn btn-primary text-center" name="connexion" type="submit" style="background: rgb(12,36,97);border-radius: 13px;border-color: rgb(12,36,97);margin: 5px;height: 39px;padding: 7px 12px;transform: scale(1.13);font-size: 14px;font-weight: bold;width: 130.344px;">Se connecter</button>
-                <div></div><small>Vous n'êtes pas encore inscrit ? <a href="./index.php?action=register">S'inscrire</a></small>
+                <div class="mb-3"><label class="form-label" for="password"><strong>Password</strong><br></label>
+                  <input class="form-control" type="password" id="password" placeholder="password" name="password"></div>
+                <div class="mb-3" style="width: 435px;height: -65px;margin: 20px;padding: 0px;"></div><button class="btn btn-primary text-center" name="connexion" type="submit" style="background: rgb(12,36,97);border-radius: 13px;border-color: rgb(12,36,97);margin: 5px;height: 39px;padding: 7px 12px;transform: scale(1.13);font-size: 14px;font-weight: bold;width: 130.344px;">Log in</button>
+                <div></div>Not yet registered? <a href="./index.php?controller=account&action=register">Register</a>
+                <div></div><a href="./index.php?controller=account&action=resetpassword">Forgot password ?</a>
             </form>
         </div>
     </section>
