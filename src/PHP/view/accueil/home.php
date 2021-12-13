2083072a7b2d01ad04a32e0a0c1a9a5a534ed423
Rayane Mallek <mallekrayane@hotmail.com>
2021-12-13 22:13:42 +0100

    fix home

--- src/PHP/view/accueil/home.php
+++ src/PHP/view/accueil/home.php
@@ -4,19 +4,3 @@
             <p>At Hack-King, we offer our best services by selling you quality hacks, guaranteed not to get banned or caught.</p>
         </div>
     </section>
-
-    <article class="example_article">
-        <p>For example, you are looking for...</p>
-        <div class="example_block">
-            <div class="lol">
-                <h1>League of Legends</h1>
-                <img class="lol_img" src="./IMG/lol.png">
-            </div>
-            <div class="lol_desc">
-                <h1>Description :</h1>
-                <p>League of Legends is a Team Strategy MOBA. Our service allows you to predict spells and their hitbox, auto attacks or movements.</p>
-                <h3>Price : 13.99$</h3>
-            </div>
-            <button class="accordion-button">Article link</button>
-        </div>
-    </article>
