74968567d98217aaefe9a83f263ac70fdb3346b8
Rayane MALLEK <rayane.mallek@etu.umontpellier.fr>
2021-12-13 08:07:24 +0100

    fix routeur pour l'affichage de la page d'accueil

--- src/PHP/controller/routeur.php
+++ src/PHP/controller/routeur.php
@@ -9,26 +9,26 @@ if (!isset($_GET['action'])) {
     $pagetitle = 'Accueil';
     require File::build_path(array("view", "view.php"));   
 } else {
-    $action = $_GET['action'];
-}
+        $action = $_GET['action'];
 
-if (!isset($_GET['controller'])) {
-    $controller = 'product';
-} else {
-    $controller = $_GET['controller'];
-}
 
-$controller_class = 'Controller' . ucfirst($controller);
+    if (!isset($_GET['controller'])) {
+        $controller = 'product';
+    } else {
+        $controller = $_GET['controller'];
+    }
 
-if (class_exists($controller_class)) {
-    $allMethodsController = get_class_methods($controller_class);
-    if (in_array($action, $allMethodsController)) {
-        $controller_class::$action();
+    $controller_class = 'Controller' . ucfirst($controller);
+
+    if (class_exists($controller_class)) {
+        $allMethodsController = get_class_methods($controller_class);
+        if (in_array($action, $allMethodsController)) {
+            $controller_class::$action();
+        } else {
+            ControllerProduct::error();
+        } 
     } else {
         ControllerProduct::error();
-    } 
-} else {
-    ControllerProduct::error();
+    }
 }
-
 ?>
